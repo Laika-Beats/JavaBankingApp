09c45676ec519d4f7a5ac43526dfc0965ab773ae
Laika-Beats <laikabeatsdenver@gmail.com>
2021-11-01 14:08:01 -0600

    previous transaction and calculate interest functions

--- JavaBankApp/src/Account.java
+++ JavaBankApp/src/Account.java
@@ -28,5 +28,27 @@ public class Account {
 			previousTransaction = -amount;
 		}
 	}
+	
+	// Function showing the previous transaction
+	void getPreviousTransaction() {
+		if (previousTransaction > 0) {
+			System.out.println("Deposited " + previousTransaction);
+		} 
+		else if (previousTransaction < 0) {
+			// Math.abs returns number that is not negative
+			System.out.println("Withdrawn: " + Math.abs(previousTransaction));
+		}
+		else {
+			System.out.println("No transaction ocurred");
+		}
+	}
+	
+	// Function calculating interest of current funds after a number of years
+	void calculateInterest (int years) {
+		double interestRate = .0185;
+		double newBalance = (balance * interestRate * years) + balance;
+		System.out.println("The current interest rate is " + (100 * interestRate) + "%");
+		System.out.println("After " + years + " years your balance will be: " + newBalance);
+	}
 
 }
