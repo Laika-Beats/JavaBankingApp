89e653813e0795a0f72ca927ea13cf01f756e1d3
Laika-Beats <laikabeatsdenver@gmail.com>
2021-11-01 13:33:53 -0600

    variables and constructor

--- JavaBankApp/src/Account.java
+++ JavaBankApp/src/Account.java
@@ -1,4 +1,16 @@
 
 public class Account {
+	
+	// Class Variables
+	int balance;
+	int previousTransaction;
+	String customerName;
+	String customerID;
+	
+	// Class Constructor
+	Account(String customerName, String customerID){
+		customerName = this.customerName;
+		customerID = this.customerID;
+	}
 
 }
